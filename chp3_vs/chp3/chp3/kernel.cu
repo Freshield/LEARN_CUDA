df97fe58f6b3263539e5683b998c4c51fa97c264
Freshield <zxdsw199182@gmail.com>
2017-01-01 00:36:23 +0800

    done test1

--- chp3_vs/chp3/chp3/kernel.cu
+++ chp3_vs/chp3/chp3/kernel.cu
@@ -3,6 +3,9 @@
 #include "device_launch_parameters.h"
 
 #include <stdio.h>
+#include <iostream>
+
+using namespace std;
 
 __global__ void kernel(int a, int b, int *c)
 {
@@ -18,6 +21,15 @@ int main(void)
 	cudaMemcpy(&c, dev_c, sizeof(int), cudaMemcpyDeviceToHost);
 	printf("2+7=%d\n",c);
 	cudaFree(dev_c);
+
+	int res;
+	int *dev_res;
+	cudaMalloc((void**)&dev_res, sizeof(int));
+	kernel << <1, 1 >> > (8, 2, dev_res);
+	cudaMemcpy(&res, dev_res, sizeof(int), cudaMemcpyDeviceToHost);
+	cout << *dev_res << endl;
+	cout << "8+2=" << res << endl;
+
 	return 0;
     
 }
