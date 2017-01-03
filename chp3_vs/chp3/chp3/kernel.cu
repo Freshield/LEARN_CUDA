d8af8b2f29900812bf71a75f186728a63e3d93b9
Freshield <zxdsw199182@gmail.com>
2017-01-03 17:30:28 +0800

    chp3 hello world

---
+++ chp3_vs/chp3/chp3/kernel.cu
@@ -0,0 +1,18 @@
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+
+__global__ void kernel(void)
+{
+    
+}
+
+int main(void)
+{
+	kernel << <1, 1 >> > ();
+	printf("Hello,World");
+	return 0;
+    
+}
