dbf8c55101c5b6622887e7bf51ef4af929349b6b
freshield <zxdsw199182@gmail.com>
2017-04-12 15:32:21 -0400

    add remarks

--- review_insight/review_chp3_2/src/review_chp3_2.cu
+++ review_insight/review_chp3_2/src/review_chp3_2.cu
@@ -16,12 +16,19 @@ __global__ void add(int a, int b, int *c){
 
 int main(void){
 	int c;
-	int *dev_c;
-	cudaMalloc( (void**)&dev_c, sizeof(int));
+	int *dev_c;//define dev_c as a pointer
+	cudaMalloc( (void**)&dev_c, sizeof(int));//get dev_c address
+	//change dev_c address to void** type
+	//allocate a address on GPU, and give the address number to dev_c
+	//
+	//all in all: dev_c is a pointer, get the dev_c address and change
+	//to void** type
 
-	add<<<1,1>>>(2, 7, dev_c);
+	add<<<1,1>>>(2, 7, dev_c);//use function and back value to
+	//the place where dev_c point
 
 	cudaMemcpy( &c, dev_c, sizeof(int), cudaMemcpyDeviceToHost);
+	//get c address, allocate the value which dev_c store and copy to c
 
 	printf( "2 + 7 = %d\n", c);
 	cudaFree(dev_c);
