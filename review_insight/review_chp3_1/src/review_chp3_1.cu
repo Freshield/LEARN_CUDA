04c9e69b71e82da27f48a264392e992c57079c11
freshield <zxdsw199182@gmail.com>
2017-04-11 20:47:24 -0400

    add samples

---
+++ review_insight/review_chp3_1/src/review_chp3_1.cu
@@ -0,0 +1,16 @@
+/*
+ ============================================================================
+ Name        : review_chp3_1.cu
+ Author      : freshield
+ Version     :
+ Copyright   : Your copyright notice
+ Description : CUDA compute reciprocals
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+int main(void){
+	printf("Hello, World!\n");
+	return 0;
+}
