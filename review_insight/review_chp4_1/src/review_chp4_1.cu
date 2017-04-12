c6feacc9ed481ce757271fdcd01c5d2ffd074b55
freshield <zxdsw199182@gmail.com>
2017-04-12 17:07:19 -0400

    done review chp4 1

--- review_insight/review_chp4_1/src/review_chp4_1.cu
+++ review_insight/review_chp4_1/src/review_chp4_1.cu
@@ -7,89 +7,31 @@
  Description : CUDA compute reciprocals
  ============================================================================
  */
+#include <stdio.h>
 
-#include <iostream>
-#include <numeric>
-#include <stdlib.h>
+#define N 10
 
-static void CheckCudaErrorAux (const char *, unsigned, const char *, cudaError_t);
-#define CUDA_CHECK_RETURN(value) CheckCudaErrorAux(__FILE__,__LINE__, #value, value)
-
-/**
- * CUDA kernel that computes reciprocal values for a given vector
- */
-__global__ void reciprocalKernel(float *data, unsigned vectorSize) {
-	unsigned idx = blockIdx.x*blockDim.x+threadIdx.x;
-	if (idx < vectorSize)
-		data[idx] = 1.0/data[idx];
+void add(int *a, int *b, int *c){
+	int tid = 0;
+	while(tid < N){
+		c[tid] = a[tid] + b[tid];
+		tid += 1;
+	}
 }
 
-/**
- * Host function that copies the data and launches the work on GPU
- */
-float *gpuReciprocal(float *data, unsigned size)
-{
-	float *rc = new float[size];
-	float *gpuData;
+int main(void){
+	int a[N], b[N], c[N];
 
-	CUDA_CHECK_RETURN(cudaMalloc((void **)&gpuData, sizeof(float)*size));
-	CUDA_CHECK_RETURN(cudaMemcpy(gpuData, data, sizeof(float)*size, cudaMemcpyHostToDevice));
+	for(int i = 0; i < N; i++){
+		a[i] = - i;
+		b[i] = i * i;
+	}
+
+	add(a, b, c);
+
+	for(int i = 0; i < N; i++){
+		printf("%d + %d = %d\n", a[i], b[i], c[i]);
+	}
 	
-	static const int BLOCK_SIZE = 256;
-	const int blockCount = (size+BLOCK_SIZE-1)/BLOCK_SIZE;
-	reciprocalKernel<<<blockCount, BLOCK_SIZE>>> (gpuData, size);
-
-	CUDA_CHECK_RETURN(cudaMemcpy(rc, gpuData, sizeof(float)*size, cudaMemcpyDeviceToHost));
-	CUDA_CHECK_RETURN(cudaFree(gpuData));
-	return rc;
-}
-
-float *cpuReciprocal(float *data, unsigned size)
-{
-	float *rc = new float[size];
-	for (unsigned cnt = 0; cnt < size; ++cnt) rc[cnt] = 1.0/data[cnt];
-	return rc;
-}
-
-
-void initialize(float *data, unsigned size)
-{
-	for (unsigned i = 0; i < size; ++i)
-		data[i] = .5*(i+1);
-}
-
-int main(void)
-{
-	static const int WORK_SIZE = 65530;
-	float *data = new float[WORK_SIZE];
-
-	initialize (data, WORK_SIZE);
-
-	float *recCpu = cpuReciprocal(data, WORK_SIZE);
-	float *recGpu = gpuReciprocal(data, WORK_SIZE);
-	float cpuSum = std::accumulate (recCpu, recCpu+WORK_SIZE, 0.0);
-	float gpuSum = std::accumulate (recGpu, recGpu+WORK_SIZE, 0.0);
-
-	/* Verify the results */
-	std::cout<<"gpuSum = "<<gpuSum<< " cpuSum = " <<cpuSum<<std::endl;
-
-	/* Free memory */
-	delete[] data;
-	delete[] recCpu;
-	delete[] recGpu;
-
 	return 0;
 }
-
-/**
- * Check the return value of the CUDA runtime API call and exit
- * the application if the call has failed.
- */
-static void CheckCudaErrorAux (const char *file, unsigned line, const char *statement, cudaError_t err)
-{
-	if (err == cudaSuccess)
-		return;
-	std::cerr << statement<<" returned " << cudaGetErrorString(err) << "("<<err<< ") at "<<file<<":"<<line << std::endl;
-	exit (1);
-}
-
