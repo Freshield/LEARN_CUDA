107b6c97cf8ccc5f81dd85617f91175a765b799b
freshield <zxdsw199182@gmail.com>
2017-04-12 16:13:37 -0400

    understand the pointer

--- review_insight/review_chp3_2_bhd/src/review_chp3_2_bhd.cu
+++ review_insight/review_chp3_2_bhd/src/review_chp3_2_bhd.cu
@@ -13,9 +13,29 @@ __global__ void add(int a, int b, int *c){
 	*c = a + b;
 }
 
+int find(int *a){
+	return *a;
+}
+
 int main(){
 	int c;
 	int *dev_c;
+
+	int b = 1;
+	int * b_ad;
+	int find_b;
+	b_ad = &b;
+	find_b = find(&b);
+	printf("b is %d\n",b);
+	printf("b address is %d\n", &b);
+	//printf("*b is %d\n", *b); error
+	printf("b_ad is %d\n", b_ad);
+	printf("&b_ad is %d\n", &b_ad);
+	printf("*b_ad is %d\n", *b_ad);
+	printf("find_b is %d\n", find_b);
+	printf("find_b address is %d\n", &find_b);
+
+
 
 	cudaMalloc((void**)&dev_c, sizeof(int));
 
